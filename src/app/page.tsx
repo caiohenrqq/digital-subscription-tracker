4a4f757e338e126c1d27b8a444500c75a03286cb
henrqq <caiohenrqq@gmail.com>
2025-03-10 13:24:48 -0400

    [public] Add Satoshi Font

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <>
-    hi
-    </>
+      <div className="font-display">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit itaque exercitationem voluptatem facere consequuntur distinctio porro vitae temporibus asperiores quam tempora, repellendus ad, aliquid, libero doloribus sunt provident nisi eaque.
+      </div>
   );
 }
